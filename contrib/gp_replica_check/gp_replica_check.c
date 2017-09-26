d6ecd5d17c097425f7173edd22f6bdf574fbfb2c
Jimmy Yih <jyih@pivotal.io>
2017-09-25 18:07:51 -0700

    make the extension more simple by making it a function that will run on the primary wrapped around an external script

--- contrib/gp_replica_check/gp_replica_check.c
+++ contrib/gp_replica_check/gp_replica_check.c
@@ -152,132 +152,32 @@ PG_FUNCTION_INFO_V1(gp_replica_check);
 Datum
 gp_replica_check(PG_FUNCTION_ARGS)
 {
+	char *primarydirpath = TextDatumGetCString(PG_GETARG_DATUM(0));
+	char *mirrordirpath = TextDatumGetCString(PG_GETARG_DATUM(1));
 
-	List  *fslist = NIL;
-	int 			proc;
-	int 			ret;
-	StringInfoData 	sqlstmt;
-	MemoryContext 	oldcontext = CurrentMemoryContext;
+	DIR *primarydir = AllocateDir(primarydirpath);
 
-	if (!gp_segment_config_has_mirrors())
+	struct dirent *dent = NULL;
+	bool equivalent = true;
+
+	while ((dent = ReadDir(primarydir, primarydirpath)) != NULL)
 	{
-		elog(NOTICE, "No mirrors exist in gp_segment_configuration. Nothing to verify.");
-		PG_RETURN_BOOL(false);
+		char primaryfilename[2000] = {'\0'};
+		bool eq = false;
+		sprintf(primaryfilename, "%s/%s", primarydirpath, dent->d_name);
+
+		char mirrorfilename[2000] = {'\0'};
+		sprintf(mirrorfilename, "%s/%s", mirrordirpath, dent->d_name);
+
+		eq = compare_files(primaryfilename, mirrorfilename, dent->d_name);
+
+		if (!eq)
+			elog(NOTICE, "Files %s and %s differ", primaryfilename, mirrorfilename);
+		equivalent &= eq;
 	}
 
-	initStringInfo(&sqlstmt);
-	appendStringInfo(&sqlstmt,
-					 "SELECT c.content, c.role, c.mode, f.fselocation "
-					 "FROM gp_segment_configuration c, pg_filespace_entry f "
-					 "WHERE c.dbid = f.fsedbid and c.content != -1 "
-					 "ORDER by c.content");
+	elog(NOTICE, "equivalent? %d", equivalent);
+	FreeDir(primarydir);
 
-	if (SPI_OK_CONNECT != SPI_connect())
-	{
-		ereport(ERROR, (errcode(ERRCODE_CDB_INTERNAL_ERROR),
-						errmsg("Write me later."),
-						errdetail("SPI_connect failed in gp_replica_check")));
-	}
-
-	ret = SPI_execute(sqlstmt.data, false, 0);
-	proc = SPI_processed;
-
-	if (ret > 0 && SPI_tuptable != NULL)
-	{
-		TupleDesc tupdesc = SPI_tuptable->tupdesc;
-		SPITupleTable *tuptable = SPI_tuptable;
-		int i;
-
-		/* We should not have any primary or mirror without a peer */
-		Assert(proc %2 == 0);
-
-		/*
-		 * Iterate through each result tuple
-		 */
-		for (i = 0; i < proc; i+=2)
-		{
-			fspair *map = (fspair *) malloc(sizeof(fspair));
-			HeapTuple 	tuple1 = tuptable->vals[i];
-			HeapTuple 	tuple2 = tuptable->vals[i+1];
-			char* role;
-			char* mode;
-			MemoryContext cxt_save;
-
-			/* use our own context so that SPI won't free our stuff later */
-			cxt_save = MemoryContextSwitchTo(oldcontext);
-
-			/* Assert that the content ids are equal */
-			Assert(*(SPI_getvalue(tuple1, tupdesc, 1)) == *(SPI_getvalue(tuple2, tupdesc, 1)));
-
-			role = SPI_getvalue(tuple1, tupdesc, 2);
-			mode = SPI_getvalue(tuple1, tupdesc, 3);
-
-			if (*mode != 's')
-			{
-				elog(ERROR, "Primary and its mirror are not in sync.");
-				PG_RETURN_BOOL(false);
-			}
-
-			if (*role == 'p')
-			{
-				map->primaryfslocation = pstrdup(SPI_getvalue(tuple1, tupdesc, 4));
-				map->mirrorfslocation = pstrdup(SPI_getvalue(tuple2, tupdesc, 4));
-			}
-			else
-			{
-				map->primaryfslocation = pstrdup(SPI_getvalue(tuple2, tupdesc, 4));
-				map->mirrorfslocation = pstrdup(SPI_getvalue(tuple1, tupdesc, 4));
-			}
-
-			fslist = lappend(fslist, map);
-
-			MemoryContextSwitchTo(cxt_save);
-		}
-	}
-
-	SPI_finish();
-	pfree(sqlstmt.data);
-
-	ListCell *l = NULL;
-	foreach(l, fslist)
-	{
-		fspair *t = (fspair *)lfirst(l);
-		elog(NOTICE, "Primary: %s | Mirror: %s", t->primaryfslocation, t->mirrorfslocation);
-
-		// Go after the base/../[0-9]+ and global/[0-9]+
-		char primaryfilepath[2000];
-		char mirrorfilepath[2000];
-
-		sprintf(primaryfilepath, "%s/base/16404", t->primaryfslocation);
-		sprintf(mirrorfilepath, "%s/base/16404", t->mirrorfslocation);
-
-		DIR *primarydir = AllocateDir(primaryfilepath);
-
-		struct dirent *dent = NULL;
-		bool equivalent = true;
-
-		while ((dent = ReadDir(primarydir, primaryfilepath)) != NULL)
-		{
-			char primaryfilename[2000] = {'\0'};
-			char primarychecksum[33] = {0};
-			bool eq = false;
-			sprintf(primaryfilename, "%s/%s", primaryfilepath, dent->d_name);
-
-			char mirrorfilename[2000] = {'\0'};
-			char mirrorchecksum[33] = {0};
-			sprintf(mirrorfilename, "%s/%s", mirrorfilepath, dent->d_name);
-
-			eq = compare_files(primaryfilename, mirrorfilename, dent->d_name);
-
-			if (!eq)
-				elog(NOTICE, "Files %s and %s differ", primaryfilename, mirrorfilename);
-			equivalent &= eq;
-		}
-
-		elog(NOTICE, "equivalent? %d", equivalent);
-
-		FreeDir(primarydir);
-	}
-
-	PG_RETURN_BOOL(true);
+	PG_RETURN_BOOL(equivalent);
 }
